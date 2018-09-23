450bd7c95160f85693754105ac4092ec2074701f
David Lam <dav_lam@ymail.com>
2018-09-22 22:31:22 -0700

    added 'chat' sidebar'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
 import React, { Component } from "react";
 import StockTracker from "./components/stocks/StockTracker";
+import { Sidebar, Menu, Segment } from "semantic-ui-react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarVisible: false
+    };
+
+    this.toggleChat = this.toggleChat.bind(this);
+    this.hideChat = this.hideChat.bind(this);
+  }
+
+  toggleChat() {
+    const isSidebarVisible = this.state.sidebarVisible;
+    this.setState({sidebarVisible: !isSidebarVisible});
+  }
+
+  hideChat() {
+    this.setState({sidebarVisible: false});
+  }
+
   render() {
-    const gridPaddingStyle = {
-      padding: "50px"
+    const menuStyle = {
+      marginTop: "0px" // have menu flushed to the top of screen
+    };
+
+    const pusherStyle = {
+      padding: "50px",
+      minHeight: window.innerHeight
     };
 
     return (
-      <div style={gridPaddingStyle}>
-        <StockTracker/>
-      </div>
+      <Sidebar.Pushable>
+        <Sidebar as={Segment.Group} 
+                 animation="push"
+                 visible={this.state.sidebarVisible}
+                 direction="right"
+                 width="very wide">
+          <Segment inverted>
+            Hello World
+          </Segment>
+        </Sidebar>
+        
+        <Menu borderless
+              style={menuStyle}>
+          <Menu.Item position="left"
+                     index={0}
+                     onClick={this.toggleChat}
+                     icon="chat"/>
+        </Menu>
+        <Sidebar.Pusher onClick={this.hideChat}
+                        style={pusherStyle}>
+          <StockTracker/>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
     );
   }
 }
